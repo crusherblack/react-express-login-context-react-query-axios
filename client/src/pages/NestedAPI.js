51d39bc167db4efa444487bf1e41e65080172743
fadhil <fadhildarma13@gmail.com>
2020-12-07 10:30:36 +0700

    done fetch

--- client/src/pages/NestedAPI.js
+++ client/src/pages/NestedAPI.js
@@ -3,15 +3,31 @@ import Navbar from "../components/Navbar";
 
 const NestedAPI = () => {
   const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const fetchUsers = async () => {
-    setLoading(true);
-    const response = await fetch("http://localhost:5000/api/v1/users");
-    const users = await response.json();
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:5000/api/v1/users");
+      const users = await response.json();
 
-    setUsers(users.data.users);
-    setLoading(false);
+      const responsePosts = await fetch("http://localhost:5000/api/v1/posts");
+      const posts = await responsePosts.json();
+
+      if (!responsePosts.ok) {
+        console.log("server error");
+        console.log("error message", posts.error.message);
+        setError(true);
+      }
+
+      setUsers(users.data.users);
+      setPosts(posts.data.posts);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -20,24 +36,42 @@ const NestedAPI = () => {
 
   console.log("state users", users);
 
+  const loadingData = loading || users.length === 0 || posts.length === 0;
+
   return (
     <>
       <Navbar />
-      {loading || !users.length === 0 ? (
-        <h1>Loading...</h1>
+      {error ? (
+        <h1>Server lagi gangguan sabar cuy</h1>
+      ) : loadingData ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <h1>Loading...</h1>
+        </div>
       ) : (
         <div className="container">
           <div className="card">
             <div className="card-header">
               <h1 className="text-center">List Users</h1>
+              {JSON.stringify(posts)}
             </div>
             <div className="card-body">
-              <ul class="list-group">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+              <ul className="list-group">
+                {users.map((user) => (
+                  <li className="list-group-item" key={user.id}>
+                    <h4>Name: {user.name}</h4>
+                    <h4 className="text-secondary">Email: {user.email}</h4>
+                    {user.skills.length > 0 &&
+                      user.skills.map((skill) => {
+                        return (
+                          skill && (
+                            <h6 key={skill.id}>
+                              {skill.id} | {skill.name}
+                            </h6>
+                          )
+                        );
+                      })}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
